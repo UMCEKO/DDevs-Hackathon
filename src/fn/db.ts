909402b51ccc33cb08f660b8ae7c120894dfb497
Souvlaki42 <souvlaki420@gmail.com>
2023-11-01 22:41:56 +0200

    All good now...

---
+++ src/fn/db.ts
@@ -0,0 +1,10 @@
+import myqsl from 'mysql2'
+import { env } from './env'
+
+export const database = myqsl.createConnection({
+	host: env.DB_HOST,
+	user: env.DB_USER,
+	password: env.DB_PASS,
+	database: env.DB_NAME,
+	port: env.DB_PORT,
+})
--- src/fn/env.ts
+++ src/fn/env.ts
@@ -1,6 +1,11 @@
-import { cleanEnv, str } from 'envalid'
+import { cleanEnv, host, port, str } from 'envalid'
 
 export const env = cleanEnv(process.env, {
 	GPT_API_KEY: str(),
 	DC_TOKEN: str(),
+	DB_HOST: host(),
+	DB_USER: str(),
+	DB_PASS: str(),
+	DB_NAME: str(),
+	DB_PORT: port(),
 })
