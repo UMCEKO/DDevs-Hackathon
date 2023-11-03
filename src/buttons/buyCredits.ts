d386c2c1f2f2c7f8c9e3fce1536aeb785c7f0dfb
Souvlaki42 <souvlaki420@gmail.com>
2023-11-03 20:32:20 +0200

    Payments Modal Test

--- src/buttons/buyCredits.ts
+++ src/buttons/buyCredits.ts
@@ -1,20 +1,80 @@
+import { StringSelectMenuBuilder } from '@discordjs/builders'
+import {
+	ActionRowBuilder,
+	ButtonInteraction,
+	ModalBuilder,
+	StringSelectMenuOptionBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} from 'discord.js'
 import { IDiscordButton } from '../fn/interfaces'
 import { createPayment } from '../fn/payments'
-import { ButtonInteraction } from 'discord.js'
 
 module.exports = {
 	name: 'buy-credits',
 	execute: async (interaction: ButtonInteraction, args) => {
+		const modal = new ModalBuilder()
+			.setTitle('Buy Credits')
+			.setCustomId('buy-credits$' + interaction.user.id)
+
+		const currencyInput = new StringSelectMenuBuilder()
+			.setPlaceholder('USD')
+			.setCustomId('currency')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel('USD')
+					.setValue('USD')
+					.setDescription('USA Dollars.'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('EUR')
+					.setValue('EUR')
+					.setDescription("Europe's common currency."),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('TRY')
+					.setValue('TRY')
+					.setDescription('Turkish Liras.'),
+			)
+
+		const quantityInput = new TextInputBuilder()
+			.setCustomId('quantity')
+			.setRequired(true)
+			.setLabel('Quantity')
+			.setStyle(TextInputStyle.Short)
+
+		const emailInput = new TextInputBuilder()
+			.setCustomId('email')
+			.setRequired(false)
+			.setLabel('Email')
+			.setStyle(TextInputStyle.Short)
+
+		const currencyRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(currencyInput)
+		const quantityRow = new ActionRowBuilder<TextInputBuilder>().addComponents(quantityInput)
+		const emailRow = new ActionRowBuilder<TextInputBuilder>().addComponents(emailInput)
+
+		// modal.addComponents(currencyRow)
+		modal.addComponents(quantityRow)
+		modal.addComponents(emailRow)
+
+		interaction.showModal(modal)
+
+		// const currency = interaction.fields.getTextInputValue('currency')
+		// const quantity = interaction.fields.getTextInputValue('quantity')
+		// const email = interaction.fields.getTextInputValue('email')
+
 		//do something
-		const userID = args[0]
-		const payment = await createPayment('USD', 1, 1000, 'umutcevdetkocak@gmail.com')
+		const payment = await createPayment('USD', 1, 0.001)
 		if (!payment.url) {
-			console.log('URL is not present')
+			console.error('URL is not present')
+			await interaction.reply({
+				content: 'Something went wrong here. Try again please.',
+				ephemeral: true,
+			})
 			return
 		}
-		let actionRow
-		await interaction.user.send(
-			"Here's your receipt. You will get your tokens once you complete the payment" + payment.url,
-		)
+		await interaction.reply({
+			content: `Here's your receipt. You will get your tokens once you complete the payment: ${payment.url}`,
+			ephemeral: true,
+		})
+		return
 	},
 } as IDiscordButton
--- src/fn/payments.ts
+++ src/fn/payments.ts
@@ -1,13 +1,13 @@
-import env from './env'
 import { Stripe } from 'stripe'
+import env from './env'
 
 const paymentClient = new Stripe(env.STRIPE_SECRET_KEY)
+type Currency = 'USD' | 'EUR' | 'TRY'
 
 export async function createPayment(
-	currency: string,
+	currency: Currency,
 	quantity: number,
 	centAmountForToken: number,
-	customer_email: string,
 ) {
 	return await paymentClient.checkout.sessions.create({
 		payment_method_types: ['card', 'paypal'],
@@ -26,8 +26,8 @@ export async function createPayment(
 		],
 		mode: 'payment',
 		allow_promotion_codes: true,
-		success_url: 'https://example.com/success',
+		success_url: 'https://umceko.github.io/success',
 		cancel_url: 'https://example.com/cancel',
-		customer_email,
+		after_expiration: { recovery: { allow_promotion_codes: true, enabled: true } },
 	})
 }
